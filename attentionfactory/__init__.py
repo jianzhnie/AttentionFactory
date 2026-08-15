a58259baff8a6fdaf5acd2827c7b8871d55e118d
jianzhnie <jianzhnie@gmail.com>
2026-08-16 00:27:55 +0800

    add  attentionfactory

--- attentionfactory/__init__.py
+++ attentionfactory/__init__.py
@@ -1,15 +1,25 @@
-"""Attention mechanisms: MHA, MQA, GQA, and MLA variants."""
+"""Attention mechanisms and educational system-level optimizations."""
 
 from .base import BaseAttention
+from .block_sparse_attention import BlockSparseAttention
 from .gqa import GroupQueryAttention
+from .linear_attention import LinearAttention
 from .mha import MultiHeadAttention
 from .mla import MultiHeadLatentAttention
 from .mqa import MultiQueryAttention
+from .paged_attention import PagedAttentionCache, PagedKVBlockAllocator, paged_attention
+from .sliding_window_attention import SlidingWindowAttention
 
 __all__ = [
     "BaseAttention",
+    "BlockSparseAttention",
     "GroupQueryAttention",
+    "LinearAttention",
     "MultiHeadAttention",
     "MultiHeadLatentAttention",
     "MultiQueryAttention",
+    "PagedAttentionCache",
+    "PagedKVBlockAllocator",
+    "SlidingWindowAttention",
+    "paged_attention",
 ]
